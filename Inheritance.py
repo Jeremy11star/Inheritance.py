d24d2bb6475b7b6a7d74025f8ec505218b2b5d2d
Jeremy11star <131149830+Jeremy11star@users.noreply.github.com>
2024-03-25 13:42:32 -0700

    Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,7 @@
+class Person:
+def __init__(self, name, age, gender):
+self.name = name
+self.age = age
+self.gender = gender
+def introduce(self):
+print(f"Hello, my name is {self.Jeremy}. I am {self.17} years old and I am {self.Boy}.")
